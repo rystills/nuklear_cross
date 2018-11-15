a1a97c0d565c4eb7d4c141e9493daec93eb91b9c
Ryan Stillings <rystills@gmail.com>
2018-11-15 03:03:23 -0500

    sdl drivers should now support right control in addition to left control

--- nuklear_drivers/nuklear_sdl_gl2.h
+++ nuklear_drivers/nuklear_sdl_gl2.h
@@ -277,29 +277,29 @@ nk_sdl_handle_event(SDL_Event *evt)
         } else if (sym == SDLK_PAGEUP) {
             nk_input_key(ctx, NK_KEY_SCROLL_UP, down);
         } else if (sym == SDLK_z)
-            nk_input_key(ctx, NK_KEY_TEXT_UNDO, down && state[SDL_SCANCODE_LCTRL]);
+            nk_input_key(ctx, NK_KEY_TEXT_UNDO, down && (state[SDL_SCANCODE_LCTRL] || state[SDL_SCANCODE_RCTRL]));
         else if (sym == SDLK_r)
-            nk_input_key(ctx, NK_KEY_TEXT_REDO, down && state[SDL_SCANCODE_LCTRL]);
+            nk_input_key(ctx, NK_KEY_TEXT_REDO, down && (state[SDL_SCANCODE_LCTRL] || state[SDL_SCANCODE_RCTRL]));
         else if (sym == SDLK_c)
-            nk_input_key(ctx, NK_KEY_COPY, down && state[SDL_SCANCODE_LCTRL]);
+            nk_input_key(ctx, NK_KEY_COPY, down && (state[SDL_SCANCODE_LCTRL] || state[SDL_SCANCODE_RCTRL]));
         else if (sym == SDLK_v)
-            nk_input_key(ctx, NK_KEY_PASTE, down && state[SDL_SCANCODE_LCTRL]);
+            nk_input_key(ctx, NK_KEY_PASTE, down && (state[SDL_SCANCODE_LCTRL] || state[SDL_SCANCODE_RCTRL]));
         else if (sym == SDLK_x)
-            nk_input_key(ctx, NK_KEY_CUT, down && state[SDL_SCANCODE_LCTRL]);
+            nk_input_key(ctx, NK_KEY_CUT, down && (state[SDL_SCANCODE_LCTRL] || state[SDL_SCANCODE_RCTRL]));
         else if (sym == SDLK_b)
-            nk_input_key(ctx, NK_KEY_TEXT_LINE_START, down && state[SDL_SCANCODE_LCTRL]);
+            nk_input_key(ctx, NK_KEY_TEXT_LINE_START, down && (state[SDL_SCANCODE_LCTRL] || state[SDL_SCANCODE_RCTRL]));
         else if (sym == SDLK_e)
-            nk_input_key(ctx, NK_KEY_TEXT_LINE_END, down && state[SDL_SCANCODE_LCTRL]);
+            nk_input_key(ctx, NK_KEY_TEXT_LINE_END, down && (state[SDL_SCANCODE_LCTRL] || state[SDL_SCANCODE_RCTRL]));
         else if (sym == SDLK_UP)
             nk_input_key(ctx, NK_KEY_UP, down);
         else if (sym == SDLK_DOWN)
             nk_input_key(ctx, NK_KEY_DOWN, down);
         else if (sym == SDLK_LEFT) {
-            if (state[SDL_SCANCODE_LCTRL])
+            if ((state[SDL_SCANCODE_LCTRL] || state[SDL_SCANCODE_RCTRL]))
                 nk_input_key(ctx, NK_KEY_TEXT_WORD_LEFT, down);
             else nk_input_key(ctx, NK_KEY_LEFT, down);
         } else if (sym == SDLK_RIGHT) {
-            if (state[SDL_SCANCODE_LCTRL])
+            if ((state[SDL_SCANCODE_LCTRL] || state[SDL_SCANCODE_RCTRL]))
                 nk_input_key(ctx, NK_KEY_TEXT_WORD_RIGHT, down);
             else nk_input_key(ctx, NK_KEY_RIGHT, down);
         } else return 0;
